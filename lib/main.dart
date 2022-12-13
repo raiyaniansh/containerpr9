f809d9d033d2ad099190dcaff23e2e5d2d4e0537
ansh <anshraiyani009@gmail.com>
2022-12-13 10:13:15 +0530

    13-12

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main()
@@ -18,11 +20,26 @@ void main()
                 height: 298,
                 alignment: Alignment.center,
                 decoration: BoxDecoration(shape: BoxShape.circle,color: Colors.blue.shade800),
-                 child: Container(
-                   height: 200,
-                   width: 200,
-                   alignment: Alignment.center,
-                   decoration: BoxDecoration(color: Colors.blue.shade700,borderRadius: BorderRadius.zero),
+                 child: Transform.rotate(
+                   angle: pi/4,
+                   child: Container(
+                     height: 210,
+                     width: 210,
+                     alignment: Alignment.center,
+                     decoration: BoxDecoration(color: Colors.blue.shade700,borderRadius: BorderRadius.only(topLeft: Radius.circular(500),bottomRight: Radius.circular(500))),
+                     child: Container(
+                       height: 87,
+                       width: 87,
+                       alignment: Alignment.center,
+                       decoration: BoxDecoration(color: Colors.blue.shade600,borderRadius: BorderRadius.only(topRight: Radius.circular(500),bottomLeft: Radius.circular(500))),
+                       child: Container(
+                         height: 30,
+                         width: 30,
+                         alignment: Alignment.center,
+                         decoration: BoxDecoration(color: Colors.blue.shade500,shape: BoxShape.circle),
+                       ),
+                     ),
+                   ),
                  ),
               ),
             ),
